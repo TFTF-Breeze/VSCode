09fdb7312df8f83fc2d66a4b8b5e802f06cde883
TFTF-Breeze <1657789778@qq.com>
2024-12-11 00:04:55 +0800

    create Tampermonkey.js

---
+++ Tampermonkey.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         西北师范大学自动刷课保存
+// @namespace    http://learning.zk211.com/
+// @version      2024-12-10
+// @description  西北师范大学自动刷课保存
+// @author       TFTF-Breeze
+// @match        http://learning.zk211.com/xbsflearning/console/*
+// @icon         http://nwnu.zk211.com/xbsf/console/images/logo60.png
+// @grant        none
+// ==/UserScript==
+
+(
+    function() {
+        'use strict';
+
+        console.log("test!!!")
+    }
+)();
