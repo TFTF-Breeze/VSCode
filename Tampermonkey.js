5bbce0dc960914f681c4c8cddea107b80f9153b8
TFTF-Breeze <1657789778@qq.com>
2024-12-11 00:17:50 +0800

    creata Tampermonkey.js

--- Tampermonkey.js
+++ Tampermonkey.js
@@ -2,7 +2,7 @@
 // @name         西北师范大学自动刷课保存
 // @namespace    http://learning.zk211.com/
 // @version      2024-12-10
-// @description  西北师范大学自动刷课保存
+// @description  西北师范大学自动刷课保存(http://learning.zk211.com/)
 // @author       TFTF-Breeze
 // @license      ABOL=1.0
 // @match        http://learning.zk211.com/xbsflearning/console/*
